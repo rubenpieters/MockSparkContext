ca58903d03a5ac90f05425e5c5ce8d3c23dbdad0
ruben <rubnepieters@gmail.com>
2016-11-02 22:19:37 +0100

    stackoverflow link for mockito benchmark behaviour

--- src/main/scala/be/rubenpieters/benchmark/MockSparkBench.scala
+++ src/main/scala/be/rubenpieters/benchmark/MockSparkBench.scala
@@ -9,6 +9,9 @@ import org.apache.spark.SparkContext
 import org.openjdk.jmh.annotations._
 
 class LocalSparkBench {
+  // could explain benchmark behaviour of mockito (see point 3):
+  // http://stackoverflow.com/a/24992961
+
   @Benchmark
   def MockedSparkContext(): Unit = {
     op1(MockSparkContext.sc)
